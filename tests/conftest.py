bc166dbdfdde9bfa609506fa3cfae33f5635c382
joey-kilgore <joey.a.kilgore@gmail.com>
2024-06-25 14:50:29 -0400

    Used the black linter

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,21 @@ import pytest
 import os
 import shutil
 
-@pytest.fixture(scope='session', autouse=True)
+
+@pytest.fixture(scope="session", autouse=True)
 def temporary_session_dir(tmpdir_factory):
     # Create a temporary directory for the entire test session
-    temp_dir = tmpdir_factory.mktemp('session-tmp')
-    
+    temp_dir = tmpdir_factory.mktemp("session-tmp")
+
     # Change the working directory to the temporary directory
     original_dir = os.getcwd()
     os.chdir(temp_dir)
-    
+
     # Yield control back to pytest
     yield temp_dir
-    
+
     # Change back to the original working directory after tests are done
     os.chdir(original_dir)
-    
+
     # Optionally clean up the temporary directory
     shutil.rmtree(temp_dir)
--- tests/test_SimLogger.py
+++ tests/test_SimLogger.py
@@ -1,9 +1,10 @@
 from SimLogger import SimLogger
 
+
 def test_save_and_get():
-    a = 'sample-object'
-    simTag = 'testing-simTag'
-    objTag = 'testing-objTag'
+    a = "sample-object"
+    simTag = "testing-simTag"
+    objTag = "testing-objTag"
     SimLogger.saveObj(simTag, objTag, a)
-    aLoaded = SimLogger.getObjectFromUniqueId(f'{simTag}_{objTag}')
-    assert(a==aLoaded)
+    aLoaded = SimLogger.getObjectFromUniqueId(f"{simTag}_{objTag}")
+    assert a == aLoaded
