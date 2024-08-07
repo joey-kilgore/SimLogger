9fb855ae7e949d311771436b27857340a2e7a151
joey-kilgore <joey.a.kilgore@gmail.com>
2024-08-07 17:12:12 -0400

    lint

--- src/SimLogger/SimNotify.py
+++ src/SimLogger/SimNotify.py
@@ -1,17 +1,18 @@
 from SimLogger import SimLogger
 import requests
 
+
 def sendNotification(text, endpoint):
     """Send notification using notify! Check the README.md for setup details.
 
     Args:
         text (str): text that will be sent as the notification (typically simTag and
-            message) 
+            message)
         endpoint (str): The channel the notifications will be sent to
 
     """
 
-    endpoint = endpoint.replace("/c/","/").strip("/")
+    endpoint = endpoint.replace("/c/", "/").strip("/")
 
     SimLogger.logNotes(f"USING NOTIFY ENDPOINT: {endpoint}")
     response = requests.post(endpoint, data=text, headers={})
